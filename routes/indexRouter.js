70db3c15b787882d6dcece242e744c326673aaac
mayorr-star <immaboat11@gmail.com>
2024-10-07 14:20:42 +0000

    Add a get route for sign-up page to display sign up form

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,10 @@
-const {Router} = require('express');
-const messagesController = require('../contollers/messagesController');
+const { Router } = require("express");
+const messagesController = require("../contollers/messagesController");
+const indexController = require('../contollers/indexController');
 
 const router = Router();
 
-router.get('/', messagesController.getAllMessages)
+router.get("/", messagesController.getAllMessages);
+router.get("/sign-up", indexController.getSignUpForm);
 
 module.exports = router;
