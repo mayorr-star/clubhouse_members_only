08ebaaf640db9ad7b76bdc353bc3499648fa69f8
mayorr-star <immaboat11@gmail.com>
2024-10-07 20:59:24 +0000

    Configure sessionStore to use postgreSql database

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-const express = require('express');
-const indexRouter = require('./routes/indexRouter');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const indexRouter = require("./routes/indexRouter");
+const path = require("path");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const pool = require("./db/pool");
+
+require("dotenv").config();
 
 const app = express();
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs');
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+const seesionStore = new pgSession({
+  pool: pool,
+  createTableIfMissing: true,
+});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    store: seesionStore,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+  })
+);
+
+app.use("/", indexRouter);
+
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
