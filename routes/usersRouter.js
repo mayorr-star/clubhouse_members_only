84f7501c8b9b56ba0c97eba3e52f9997cc45f052
mayorr-star <immaboat11@gmail.com>
2024-10-09 17:09:49 +0000

    Add a sign in route and authenticate user when he / she signs in

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
-const passport = require("passport");
 const userController = require("../controllers/userController");
+const passport = require('passport');
+require('../passport/passport');
 const router = Router();
 
 router.get("/sign-up", userController.getSignUpForm);
 router.post("/sign-up", userController.createUser);
 router.get("/sign-in", userController.getSignInForm);
+router.post('/sign-in', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/sign-in'
+}))
 router.get("/become-a-member", userController.getEntryPage);
+
+module.exports = router;
