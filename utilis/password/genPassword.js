d46f3f63fb6bf19e5e63a3532826d4a78cfd0153
mayorr-star <immaboat11@gmail.com>
2024-10-09 17:15:58 +0000

    Change salt value from 32 to 10

--- utilis/password/genPassword.js
+++ utilis/password/genPassword.js
@@ -1,13 +1,12 @@
-const bcrypt = require('bcryptjs');
-const salt = 32;
+const bcrypt = require("bcryptjs");
+const salt = 10;
 const generatePassword = (password) => {
   const hash = bcrypt.hashSync(password, salt);
   return hash;
 };
 
 const verifyPassword = (password, hash) => {
-  const hashVerify = bcrypt.hashSync(password, salt);
-  return hash === hashVerify;
+  return bcrypt.compareSync(password, hash);
 };
 
 module.exports = { generatePassword, verifyPassword };
