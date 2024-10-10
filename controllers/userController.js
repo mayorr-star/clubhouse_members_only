10d483bb122ae455205a23012df7bec0a8670460
mayorr-star <immaboat11@gmail.com>
2024-10-10 19:19:13 +0000

    Add controller to user membership status when they become users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,9 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
-const getYear = require("../utilis/year");
+const {getYear} = require("../utilis/year");
 const db = require("../db/queries");
 const { generatePassword } = require("../utilis/password/genPassword");
+require("dotenv").config();
 
 const year = getYear();
 
@@ -53,17 +54,48 @@ const createUser = [
   }),
 ];
 
-
 const getEntryPage = asyncHandler((req, res) => {
   res.render("join", { year: year, userlogin: false, admin: false });
 });
 
 const getSignUpForm = asyncHandler((req, res) => {
-  res.render("signupForm", { year: year, userlogin: false, admin: false });
+  res.render("signupForm", { year: year, admin: false, user: Boolean(req.user), member: false });
 });
 
 const getSignInForm = asyncHandler((req, res) => {
-  res.render("signInForm", { year: year, userlogin: false, admin: false });
+  res.render("signInForm", { year: year, admin: false, user: Boolean(req.user), member: false });
 });
 
-module.exports = { getEntryPage, getSignInForm, getSignUpForm, createUser };
+const validateSecretCode = [
+  body("password")
+    .custom((value) => {
+      return value === process.env.MEMBERSHIP_SECRET_CODE;
+    })
+    .withMessage("Sorry incorrect passcode"),
+];
+
+const changeMembershipStatus = [
+  validateSecretCode,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("join", {
+        errors: errors.array(),
+        year: year,
+        userlogin: false,
+        admin: false,
+      })
+    };
+    const messages = await db.getAllMessages()
+    res.render("messages", { year: year, userlogin: false, admin: false, messages: messages });
+    await db.updateMembershipStatus('TRUE', req.user.id);
+  }),
+];
+
+module.exports = {
+  getEntryPage,
+  getSignInForm,
+  getSignUpForm,
+  createUser,
+  changeMembershipStatus,
+};
