3c7f731a89b9978bc44c3426382677d227973241
mayorr-star <immaboat11@gmail.com>
2024-10-10 19:17:27 +0000

    Add code to check where user is a member or not
    Display button to become a member if user is not

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,12 @@
-const getYear = require("../utilis/year");
+const { getYear } = require("../utilis/year");
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 
 const year = getYear();
 
 const renderHomePage = asyncHandler(async (req, res) => {
-    res.render('index', { year: year, userlogin: false, admin: false })
+    const member = req.user ? (req.user.membership_status || false) : false;
+    res.render('index', { year: year, user: Boolean(req.user), admin: false, member: member })
 });
 
 module.exports = { renderHomePage};
