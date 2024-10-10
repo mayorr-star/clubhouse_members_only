6b1b20be408682cac2bb6a870173862a8e336db6
mayorr-star <immaboat11@gmail.com>
2024-10-10 19:12:55 +0000

    Initial commit

---
+++ routes/messageRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const messagesController = require('../controllers/messagesController');
+const {isAuthenticated} = require('../controllers/authenticationController');
+
+const router = Router();
+
+router.get('/', isAuthenticated, messagesController.getAllMessages);
+router.get('/add-new-message', messagesController.renderMessageForm);
+router.post('/add-new-message', messagesController.createNewMessage);
+
+module.exports = router;
