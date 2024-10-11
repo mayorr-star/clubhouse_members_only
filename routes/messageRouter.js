db9427fffde02808be3922ba606e9660db718ed6
mayorr-star <immaboat11@gmail.com>
2024-10-11 16:16:49 +0000

    Add post delete route

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -7,5 +7,6 @@ const router = Router();
 router.get('/', isAuthenticated, messagesController.getAllMessages);
 router.get('/add-new-message', messagesController.renderMessageForm);
 router.post('/add-new-message', messagesController.createNewMessage);
+router.post('/delete/:messageId', messagesController.deleteMessage)
 
 module.exports = router;
