73c217736ac4a074bdfb7771616c4f749f4d7592
mayorr-star <immaboat11@gmail.com>
2024-10-11 16:10:20 +0000

    Add code to set admin to false if user is not looged in, else set admin to req.user.admin property

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,7 +6,8 @@ const year = getYear();
 
 const renderHomePage = asyncHandler(async (req, res) => {
     const member = req.user ? (req.user.membership_status || false) : false;
-    res.render('index', { year: year, user: Boolean(req.user), admin: false, member: member })
+    const admin = req.user ? (req.user.admin || false) : false;
+    res.render('index', { year: year, user: Boolean(req.user), admin: admin, member: member })
 });
 
 module.exports = { renderHomePage};
