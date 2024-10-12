94fe000b549ea4f8e838789f98714d5be0990ec5
mayorr-star <immaboat11@gmail.com>
2024-10-12 20:59:38 +0000

    Refactor to make code more readable

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,9 +1,11 @@
 const isAuthenticated = (req, res, next) => {
-  if (!req.isAuthenticated())
+  if (!req.isAuthenticated()) {
     return res
-      .status(401)
-      .json({ msg: "You are not authorized to view this resource" });
-  next();
+    .status(401)
+    .json({ msg: "You are not authorized to view this resource" });
+  } else {
+    next();
+  }
 };
 
 module.exports = { isAuthenticated };
