c4fbd1927f2fe4cc7d09a308a43f0f1e90ab8589
mayorr-star <immaboat11@gmail.com>
2024-10-14 21:01:29 +0000

    Add ssl

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,6 @@
 const { Pool} = require('pg');
 require('dotenv').config();
 
-let connectionURI = null;
-
-if (process.env.NODE_ENV === 'development') {
-    connectionURI = process.env.LOCAL_DATABASE_URL;
-} else {
-    connectionURI = process.env.PUBLIC_DATABASE_URL;
-}
-
 module.exports = new Pool({
-    connectionString: connectionURI
+    ssl: process.env.NODE_ENV === "production"
 })
--- db/populatedb.js
+++ db/populatedb.js
@@ -13,14 +13,14 @@ CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY GENERATED ALWAYS AS 
 `;
 
 async function main() {
-    console.log("seeding...");
-    const client = new Client({
-      connectionString: process.argv[2]
-    });
-    await client.connect();
-    await client.query(SQL);
-    await client.end();
-    console.log("done");
-  }
-  
-  main();
+  console.log("seeding...");
+  const client = new Client({
+    ssl: process.env.NODE_ENV === "production",
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
