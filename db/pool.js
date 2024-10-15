aa08781e778f8b9e84ac43af9b20214b33c47813
mayorr-star <immaboat11@gmail.com>
2024-10-15 10:31:10 +0000

    Refactor

--- db/pool.js
+++ db/pool.js
@@ -1,15 +1,14 @@
 const { Pool } = require("pg");
-require("dotenv").config();
+require('dotenv').config()
+
+let connection = null;
 
 if (process.env.NODE_ENV === "development") {
-  module.exports = new Pool({
-    connectionString: process.env.LOCAL_DATABASE_URL,
-  });
+  connection = process.env.LOCAL_DATABASE_URL;
 } else {
-  module.exports = new Pool({
-    connectionString: process.env.EXTERNAL_DATABASE_URL,
-    ssl: {
-      require: true,
-    },
-  });
+  connection = process.env.EXTERNAL_DATABASE_URL;
 }
+
+module.exports = new Pool({
+  connectionString: connection,
+});
